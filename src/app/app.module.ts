03291e0fd23e8d15c7d8bb5ec78b0a65c77e9747
Ryan_Truong <ryanhoang32@gmail.com>
2023-02-05 22:34:21 -0600

    renamed files to match angular standards and created recipes model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ShoppingListComponent } from './components/shopping-list/shopping-list-component.component'
-import { RecipeBookComponent } from './components/recipe-book/recipe-book-component.component';
-import { HeaderComponent } from './components/header/header-component.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list-component'
+import { RecipeBookComponent } from './components/recipe-book/recipe-book-component';
+import { HeaderComponent } from './components/header/header-component';
 import { RecipeListComponent } from './components/recipe-book/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './components/recipe-book/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './components/recipe-book/recipe-list/recipe-item/recipe-item.component';
--- src/app/components/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,10 +1,20 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  reciples: Recipe[] = [
+    new Recipe('A test reciple', 'This is simply a test', 'https://www.google.com/search?q=recipe&rlz=1C1CHBF_enUS924US924&sxsrf=AJOqlzVrAmM03mA71H2aZiFrKV03H29X0g:1675657633719&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiBgJeQh4D9AhV7jokEHdO6Dn0Q_AUoAXoECAEQAw&biw=2560&bih=1297&dpr=1#imgrc=v4k6d-HqJChBPM')
+  ];
 
+  constructor() { }
+
+  ngOnInit() {
+
+  }
 }
